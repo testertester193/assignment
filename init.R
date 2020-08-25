8d198a6b08548e84383b8ddeb7c5a5d1ea90db9e
testertester193 <testertester193@gmail.com>
2020-08-25 09:04:15 +0100

    dndndbdndbnabsnsanbs

---
+++ init.R
@@ -0,0 +1,11 @@
+my_packages = c("plyr","installr","shiny","rattle","shinythemes","leaflet","dplyr")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
