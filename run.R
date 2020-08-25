ea0f490575d25174f9967b267e6f05eb8d31abb0
testertester193 <testertester193@gmail.com>
2020-08-25 09:02:55 +0100

    nnasnasnmnmas

--- run.R
+++ run.R
@@ -1,17 +1,3 @@
-my_packages = c("plyr","installr","shiny","rattle","shinythemes","leaflet","dplyr")
-
-###########################################################
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
-
 library(shiny)
 port <- Sys.getenv('PORT')
 shiny::runApp(
